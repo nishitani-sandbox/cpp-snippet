e7c1dc69b0ab9aa7d275de1f586fd49459ebe29e
nishitaniyuki <yuki-nishitani@cybozu.co.jp>
2021-07-15 01:29:30 +0000

    add delegating_constructor

---
+++ delegating_constructor/main.cpp
@@ -0,0 +1,29 @@
+struct S {
+  int i;
+
+  S()
+    : i(3) {}
+
+  S(int i)
+    : i(i)
+  {
+    std::cout << "constructor called\n"s;
+  }
+
+  S(S const & other)
+    : S(other.i)
+  {
+    std::cout << "delegating constructor called\n"s;
+  }
+};
+
+int main()
+{
+  S a(1);
+  S b = a;
+  std::cout << "b.i: " << b.i << "\n"s;
+  // S c() does not work!
+  S c;
+  c.i = 3;
+  std::cout << "c.i: " << c.i << "\n"s;
+}
