74dc765f0c33c6fd5d45084af05df3e9dcfd290d
nishitaniyuki <yuki-nishitani@cybozu.co.jp>
2021-07-16 01:28:20 +0000

    update array_iterator

--- array_iterator/main.cpp
+++ array_iterator/main.cpp
@@ -7,9 +7,14 @@ struct array_iterator
   array_iterator(Array & a, std::size_t i)
     : a(a), i(i) {}
 
-  typename Array::reference operator *()
+  typename Array::reference operator *() const
   {
     return a[i];
+  }
+
+  typename Array::reference operator [](std::size_t n) const
+  {
+    return *(this + n);
   }
 
   array_iterator & operator ++()
@@ -38,16 +43,6 @@ struct array_iterator
     return copy;
   }
 
-  bool operator ==(array_iterator const & right)
-  {
-    return i == right.i;
-  }
-
-  bool operator !=(array_iterator const & right)
-  {
-    return i != right.i;
-  }
-
   array_iterator & operator +=(std::size_t n)
   {
     i += n;
@@ -61,9 +56,14 @@ struct array_iterator
     return copy;
   }
 
-  typename Array::reference operator [](std::size_t n) const
+  bool operator ==(array_iterator const & right) const
   {
-    return *(this + n);
+    return i == right.i;
+  }
+
+  bool operator !=(array_iterator const & right) const
+  {
+    return i != right.i;
   }
 
   bool operator < (array_iterator const & right) const
@@ -88,6 +88,99 @@ struct array_iterator
   }
 };
 
+template <typename Array>
+struct array_const_iterator
+{
+  Array const & a;
+  std::size_t i;
+
+  array_const_iterator(Array const & a, std::size_t i)
+    : a(a), i(i) {}
+
+  array_const_iterator(typename array_iterator<Array>::iterator const & iter)
+    : a(iter.a), i(iter.i) {}
+
+  typename Array::const_reference operator *() const
+  {
+    return a[i];
+  }
+
+  typename Array::const_reference operator [](std::size_t n) const
+  {
+    return *(this + n);
+  }
+
+  array_const_iterator & operator ++()
+  {
+    ++i;
+    return *this;
+  }
+
+  array_const_iterator operator ++(int)
+  {
+    array_iterator copy = *this;
+    ++*this;
+    return copy;
+  }
+
+  array_const_iterator & operator --()
+  {
+    --i;
+    return *this;
+  }
+
+  array_const_iterator operator --(int)
+  {
+    array_iterator copy = *this;
+    --*this;
+    return copy;
+  }
+
+  array_const_iterator & operator +=(std::size_t n)
+  {
+    i += n;
+    return *this;
+  }
+
+  array_const_iterator operator +(std::size_t n) const
+  {
+    auto copy = *this;
+    copy += n;
+    return copy;
+  }
+
+  bool operator ==(array_const_iterator const & right) const
+  {
+    return i == right.i;
+  }
+ 
+  bool operator !=(array_const_iterator const & right) const
+  {
+    return i != right.i;
+  }
+
+  bool operator < (array_const_iterator const & right) const
+  {
+    return i < right.i;
+  }
+
+  bool operator <= (array_const_iterator const & right) const
+  {
+    return i <= right.i;
+  }
+
+
+  bool operator > (array_const_iterator const & right) const
+  {
+    return i > right.i;
+  }
+
+  bool operator >= (array_const_iterator const & right) const
+  {
+    return i >= right.i;
+  }
+};
+
 
 template <typename T, std::size_t N>
 struct array
@@ -97,6 +190,7 @@ struct array
   using const_reference = T const &;
   using size_type = std::size_t;
   using iterator = array_iterator<array>;
+  using const_iterator = array_const_iterator<array>;
 
   value_type storage[N];
 
@@ -119,6 +213,16 @@ struct array
   {
     return iterator(*this, N);
   }
+
+  const_iterator cbegin()
+  {
+    return const_iterator(*this, 0);
+  }
+
+  const_iterator cend()
+  {
+    return const_iterator(*this, N);
+  }
 };
 
 int main()
@@ -127,4 +231,9 @@ int main()
   std::for_each(std::begin(a), std::end(a), [](auto x) {
     std::cout << x << "\n"s;
   });
+
+  array<int, 5> b = {1, 2, 3, 4, 5};
+  auto i = b.begin();
+  *i = 100;
+  std::cout << *i << "\n"s;
 }
