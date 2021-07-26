e0aa84cd46c6800659ab28de8c6a4ab570b12ef0
nishitaniyuki <yuki-nishitani@cybozu.co.jp>
2021-07-26 07:18:11 +0000

    update dynamic_cast

--- dynamic_cast/main.cpp
+++ dynamic_cast/main.cpp
@@ -2,22 +2,32 @@ class A
 {
   public:
     virtual ~A() = default;
-    void foo() { std::cout << "called by A\n"s; }
+    virtual void foo() { std::cout << "called by A\n"s; }
 };
 
 class B : public A
 {
   public:
-    void foo() { std::cout << "called by B\n"s; }
+    virtual void foo() { std::cout << "called by B\n"s; }
 };
 
-A* genA() {
+A* genA()
+{
   static A a;
   return &a;
 }
 
+B* genB()
+{
+  static B b;
+  return &b;
+}
+
 int main()
 {
-  auto b = dynamic_cast<B*>(genA());
-  b->foo();
+  A* ia = dynamic_cast<A*>(genB());
+  ia->foo();
+
+  B* ib = dynamic_cast<B*>(genA());
+  ib->foo();
 }
