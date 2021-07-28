b80540921f81bf97d5297c43f73fd73aa8bb78bb
nishitaniyuki <yuki-nishitani@cybozu.co.jp>
2021-07-28 01:42:25 +0000

    update virtual

--- virtual/main.cpp
+++ virtual/main.cpp
@@ -1,18 +1,27 @@
 class A
 {
   public:
+    virtual ~A() {
+      std::cout << "A's destructor\n"s;
+    }
     virtual void foo() = 0;
 };
 
 class B : public A
 {
   public:
+    ~B() {
+      std::cout << "B's destructor\n"s;
+    }
     void foo() { std::cout << "called by B\n"s; }
 };
 
 class C : public B
 {
   public:
+    ~C() {
+      std::cout << "C's destructor\n"s;
+    }
     void foo() { std::cout << "called by C\n"s; }
 };
 
