9ee9956b790f58d036668fbb67c627ce08c392d7
nishitaniyuki <yuki-nishitani@cybozu.co.jp>
2021-09-24 14:27:14 +0000

    update gtest

--- gtest/hello_test.cpp
+++ gtest/hello_test.cpp
@@ -1,4 +1,32 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "all.h"
+
+class Turtle
+{
+  public:
+    virtual ~Turtle() {}
+    virtual void PenUp() = 0;
+    virtual void PenDown() = 0;
+    virtual void Forward(int distance) = 0;
+    virtual void Turn(int degrees) = 0;
+    virtual void GoTo(int x, int y) = 0;
+    virtual int GetX() const = 0;
+    virtual int GetY() const = 0;
+};
+
+class MockTurtle : public Turtle
+{
+  public:
+    MOCK_METHOD(void, PenUp, (), (override));
+    MOCK_METHOD(void, PenDown, (), (override));
+    MOCK_METHOD(void, Forward, (int distance), (override));
+    MOCK_METHOD(void, Turn, (int degrees), (override));
+    MOCK_METHOD(void, GoTo, (int x, int y), (override));
+    MOCK_METHOD(int, GetX, (), (const, override));
+    MOCK_METHOD(int, GetY, (), (const, override));
+};
+
 
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
@@ -7,4 +35,3 @@ TEST(HelloTest, BasicAssertions) {
   // Expect equality.
   EXPECT_EQ(7 * 6, 42);
 }
-
